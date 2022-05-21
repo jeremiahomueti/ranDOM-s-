0d9534c40c3187f9f21e35db99be69172050dfcd
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-21 18:49:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// there is a simple to create random decimal numbers in JavaScript. It is with the Maths.Random Function.
+
+var randomFractions = "";
+
+for (i = 0; i > 0 && i < 5; i ++)
+{
+console.log(math.random(i));
+}
+
